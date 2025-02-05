c449c13d9dd8843e8ba94cd9b524c775bb319174
aprilsmadi <asmadi@credicare.com>
2025-02-05 09:02:58 -0800

    fixed backed and additional files
    combined the server files  into one and installed the MUI libraries needed

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,65 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser'); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const My_APIkey ="avw1EWKRNMTf6rfZaenENLPB0JrjCbrMm6peowyM"
+
+
+app.get('/api/random-quote', async (req, res) => {
+    try {
+      const response = await axios.get('https://dummyjson.com/quotes/random');
+      const quote = response.data; 
+      res.json({ text: quote.quote , author: quote.author }); 
+      console.log(` this is res in backend ${quote}`)
+      console.log(quote)
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+      res.status(500).json({ message: 'Failed to fetch quote' });
+    }
+  });
+
+
+
+
+//API for exercises  WE MUST USE ANOTHER API  :
+app.post("/exercises", async(req,res)=>{
+    const { difficulty } = req.body; 
+    console.log(`Requested difficulty: ${difficulty}`);
+    const response = await axios.get(`https://api.api-ninjas.com/exercises?difficulty=${difficulty}`, {
+        headers: {
+          'X-Api-Key': "avw1EWKRNMTf6rfZaenENLPB0JrjCbrMm6peowyM",
+        }})
+
+        
+  
+    try {
+      // Log the response for debugging purposes
+      console.log('API Response Data:', response.data);
+  
+      // Send the response data to the client
+      res.send(JSON.stringify(response.data));
+      }
+  
+      
+     catch (error) {
+      // Handle errors
+      if (error.response) {
+        // If the error is from the API call (like a 400 or 500 status)
+        console.error('API Error:', error.response.data);
+        res.status(error.response.status).json({ error: error.response.data });
+      } else {
+        // Handle other errors (network error, etc.)
+        console.error('General Error:', error.message);
+        res.status(500).json({ error: error.message });
+      }
+    }
+  });
+
 
 // Connect to SQLite Database
 const db = new sqlite3.Database('./users.db', (err) => {
