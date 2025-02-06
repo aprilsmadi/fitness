8bf90ba709ec7cecb128a81b128ff0e27daa7cd6
Lilly <lasfoor@credicare.com>
2025-02-06 18:51:34 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express ()
-const port = 7000
+const port = 8000
 const cors = require("cors")
 const axios = require("axios")
 const bodyParser = require('body-parser'); 
